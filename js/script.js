5e776fb161aaf430c7930f194429cb53e648b5e0
valerio <valerio.deprosperi@gmail.com>
2020-08-26 20:26:01 +0200

    stampa fizzbuzz se sono multipli di 3e5

--- js/script.js
+++ js/script.js
@@ -5,11 +5,17 @@
 // 1. creo ciclo for per fare numeri da 1 a 100
 for (var i = 0; i < 100; i++) {
   console.log([i]);
-// 2.per i multipli di 3 faccio uscire Fizz,quindi uso il modulo/resto,lo stesso per i multipli di 5
-if(i % 3 == 0){
-  document.getElementById('list').innerHTML += '<li>' + "Fizz" + '</li>';
-}else if (i % 5 == 0){
-  document.getElementById('list').innerHTML += '<li>' + "Buzz" + '</li>';
-  // prova push gitkraken
-}
+// 2.per i multipli di 3 faccio uscire Fizz,quindi uso il modulo/resto,lo stesso per i multipli di 5 e se sono uguali esce FizzBuzz
+if (i % 3 == 0 && i % 5 == 0) {
+        document.getElementById('list').innerHTML += '<li>' + 'FizzBuzz' + '</li>';
+    }
+    else if (i % 3 == 0) {
+        document.getElementById('list').innerHTML += '<li>' + 'Fizz' + '</li>';
+    }
+    else if (i % 5 == 0) {
+        document.getElementById('list').innerHTML += '<li>' + 'Buzz' + '</li>';
+    }
+    else {
+        document.getElementById('list').innerHTML += '<li>' + i + '</li>';
+    }
 }
